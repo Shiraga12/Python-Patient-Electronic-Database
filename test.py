bf51374e9fb42913da5cfef3dc5e2ede04ac7b21
Toluwani D. Adeoti <137415857+Shiraga12@users.noreply.github.com>
2025-07-14 12:21:46 -0400

    Changes to be committed for Project:

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+# Hello, my name is Toluwani Adeoti.
+#
+# I'm sorry, my computer didn't come with a microphone.
+# But the show must go on.
+# This is the Python Patient Electronic Database.
+
+# This is the testing python file.
+# It allows the user to type in the patient's information. 
+# # For demo purposes, I'm only allowimg the first name, last name, and ID for myself to type in.
+
 from patient import Patient as PD
 from patient import Billing as BD
 import pandas as pd
@@ -6,18 +16,21 @@ PATIENT = []
 Continue = "T"
 
 while Continue == "T" or Continue == "t":
-    Firstname = input("Enter patient's first name: ")
-    Lastname = input("Enter patient's last name: ")
-    ID = input("Enter patient's ID: ")
-    PATIENT.append(PD(Firstname,Lastname,ID))
-    Continue = input("Continue? T = yes, F = no ")
+    Firstname = input("Enter patient's first name: ") # First, I type in the patient's first name...
+    Lastname = input("Enter patient's last name: ") # Then, the patient's last name...
+    ID = int(input("Enter patient's ID: ")) # Finally the ID.
+    print(f"{ID}: {Firstname} {Lastname}") # This will display the information given so far.
+    PATIENT.append(PD(Firstname,Lastname,ID)) # It then goes to the PATIENT array.
+    Continue = input("Continue? T = yes, F = no ") # Next, it asks me if I want to make another one. If so, the process will start all over again. If not, It will print out end, and...
     if Continue == "F" or Continue == "f":
-        print("\nEnd")
+        print("\nEnd\n")
         break;
 
-PATIENT_DF = pd.DataFrame(columns=["First Name","Last Name", "ID"])
+PATIENT_DF = pd.DataFrame(columns=["First Name","Last Name", "ID"]) # It makes a DataFrame via PANDAS.
 for i in range(len(PATIENT)):
     PATIENT_DF.loc[i+1,"First Name"] = PATIENT[i].getFirstName()
     PATIENT_DF.loc[i+1,"Last Name"] = PATIENT[i].getLastName()
     PATIENT_DF.loc[i+1,"ID"] = PATIENT[i].getID()
 print(PATIENT_DF)
+
+
